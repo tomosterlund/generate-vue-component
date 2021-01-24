d649d527a1aee9a90dae6ddd181311de385e7390
Tom Österlund <tom.osterlund1@gmail.com>
2021-01-24 14:07:40 +0100

    adds default filepaths for nuxt project structure

--- handlers/CreateVueComponent.js
+++ handlers/CreateVueComponent.js
@@ -2,19 +2,19 @@ const fs = require('fs');
 const componentTemplate = require('../templates/ComponentTemplate');
 
 const CreateVueComponent = async (fileName, options) => {
-    let data;
-    let filePath = `${fileName}.vue`;
-    const filePathInComponentsDir = await checkIfComponentsDirExists('./src', fileName);
-    
-    if (filePathInComponentsDir) {
-        filePath = filePathInComponentsDir;
-    }
-
     if (options.help) {
         return console.log(`The following flags are available:
             -d                adds a data property to the component
             -m                adds a methods property
+            -s                sets the style-lang attribute to SCSS
         `);
+    }
+
+    let data;
+    let filePath = `${fileName}.vue`;
+    const filePathInComponentsDir = await returnFilePathInComponentsDir(fileName);
+    if (filePathInComponentsDir) {
+        filePath = filePathInComponentsDir;
     }
 
     data = new Uint8Array(Buffer.from(componentTemplate(options.data, options.methods, options.scss)));
@@ -26,18 +26,41 @@ const CreateVueComponent = async (fileName, options) => {
 }
 
 /**
+ * If there is an src/components or /components dir,
+ * this function returns that filepath
+ * @param {string} fileName
+ */
+const returnFilePathInComponentsDir = async (fileName) => {
+    let filePath;
+    // If an './src/components' directory exists, set filePath to that directory
+    const filePathInSrcComponentsDir = await checkIfComponentsDirExists('./src', './src/components/', fileName);
+    if (filePathInSrcComponentsDir) {
+        filePath = filePathInSrcComponentsDir;
+    }
+
+    // If a './components' directory exists, set filePath to that directory
+    const filePathInComponentsDir = await checkIfComponentsDirExists('./', './components/', fileName);
+    if (filePathInComponentsDir) {
+        filePath = filePathInComponentsDir;
+    }
+
+    return filePath;
+}
+
+/**
  * If a components directory exists, the filepath is set to this directory
- * @param {string} path 
+ * @param {string} path the path to check in, if a directory exists
+ * @param {string} newFilePath the path to be inserted when creating a file
  * @param {string} modulename 
  */
-const checkIfComponentsDirExists = async (path, modulename) => {
-    const srcExists = fs.existsSync(path);
-    if (srcExists) {
+
+const checkIfComponentsDirExists = async (path, newFilePath, modulename) => {
+    const srcCompExists = fs.existsSync(path);
+    if (srcCompExists) {
         const dir = await fs.promises.opendir(path);
         for await (const dirent of dir) {
             if (dirent.name === 'components') {
-                let filePath = `./src/components/${modulename}.vue`;
-                return filePath;
+                return `${newFilePath}${modulename}.vue`;
             }
         }
     }
--- handlers/CreateVuexModule.js
+++ handlers/CreateVuexModule.js
@@ -2,11 +2,7 @@ const fs = require('fs');
 const vuexModuleTemplate = require('../templates/vuex-module');
 
 const CreateVuexModule = async (modulename) => {
-    let filePath = `${modulename}.js`;
-    const filePathInStore = await checkIfStoreExists('./src', modulename);
-    if (filePathInStore) {
-        filePath = filePathInStore;
-    }
+    const filePath = await returnFilePathInStoreDir(modulename);
     const moduleCode = vuexModuleTemplate(modulename);
     fs.writeFile(filePath, moduleCode, err => {
         if (err) throw err;
@@ -15,18 +11,37 @@ const CreateVuexModule = async (modulename) => {
 }
 
 /**
+ * Returns the new filepath in a store directory if it exists
+ * If it does not exists, it returns a filepath in CWD
+ * @param {string} modulename
+ * @returns {string}
+ */
+const returnFilePathInStoreDir = async (modulename) => {
+    let filePath = `${modulename}.js`;
+    const filePathInSrcStore = await checkIfStoreExists('./src', './src/store/', modulename);
+    if (filePathInSrcStore) {
+        filePath = filePathInSrcStore;
+    }
+    const filePathInStore = await checkIfStoreExists('./', './store/', modulename);
+    if (filePathInStore) {
+        filePath = filePathInStore;
+    }
+    return filePath;
+};
+
+/**
  * If a store directory exists, the filepath is set to this directory
  * @param {string} path 
+ * @param {string} newFilePath
  * @param {string} modulename 
  */
-const checkIfStoreExists = async (path, modulename) => {
+const checkIfStoreExists = async (path, newFilePath, modulename) => {
     const srcExists = fs.existsSync(path);
     if (srcExists) {
         const dir = await fs.promises.opendir(path);
         for await (const dirent of dir) {
             if (dirent.name === 'store') {
-                let filePath = `./src/store/${modulename}.js`;
-                return filePath;
+                return `${newFilePath}${modulename}.js`;
             }
         }
     }
